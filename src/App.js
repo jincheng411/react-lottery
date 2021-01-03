6858b92d00acf6ac09ca35eaa8004e2f702113bb
Jin Cheng <jincheng411@gmail.com>
2021-01-02 22:06:56 -0500

    finished lottery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Lottery from './Lottery';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App"></div>
+      <div className="App">
+        <Lottery />
+        <Lottery title="Mini Lotto" maxNum={10} maxBalls={4}/>
+      </div>
     )
   }
 }
